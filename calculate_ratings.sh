800306ba16adb24712a778e3bd2438e29f4b677c
Daniel J. Pritchett <dpritchett@gmail.com>
2011-09-08 11:46:25 -0500

    split out calculate and print winner functions

--- calculate_ratings.sh
+++ calculate_ratings.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
+DATE="`date +\"%d-%b-%Y\"`"
 SAGARIN_URL='http://www.usatoday.com/sports/sagarin/fbt11.htm'
 SAGARIN_FILE='fbt11.htm'
-OUTFILE_NAME='newest_picks.txt'
+OUTFILE_NAME="picks-$DATE.txt"
 
 rm $SAGARIN_FILE
 wget -q $SAGARIN_URL
 ack-grep -h "^\s+\d" | cut -c 6-60 | sed "s/=.*>//" | cut --complement -c 23-24 > ratings.txt
 
-ruby pick_winners.rb | tee $OUTFILE_NAME | mail $MY_EMAIL_ADDRESS --subject="Weekly Picks"
-cat $OUTFILE_NAME
+ruby pick_winners.rb | tee $OUTFILE_NAME
+uuencode $OUTFILE_NAME $OUTFILE_NAME | mail -s "Weekly Picks" $MY_EMAIL_ADDRESS
 rm  $OUTFILE_NAME
--- pick_winners.rb
+++ pick_winners.rb
@@ -3,11 +3,12 @@
 #
 # Will fail if ratings.txt and slate.txt are not present.
 require 'text'
+require 'active_support/core_ext'
 
 def load_ratings()
   ratings = open('ratings.txt').readlines
   ratings = ratings.map { |x| x[0..29].rstrip.lstrip.gsub(/(\s)(\d)/, ':\2').split(':') }
-  ratings = ratings.each.map { |el| [el[0].lstrip.rstrip, el[1]] }
+  ratings = ratings.each.map { |el| [el.first.lstrip.rstrip, el.second] }
   ratings.pop
   return ratings
 end
@@ -16,24 +17,16 @@ def load_slate()
   slate = open('slate.txt').readlines
   slate = slate.map.with_index do |line, index|
     line = line.rstrip.lstrip.split('@')
-    { home: line[0], away: line[1], index: index }
+    { home: line.first, away: line.second, index: index }
   end
-end
-
-def print_slate(slate)
-  slate.each { |game| puts "#{game[0]} at #{game[1]}" }
-end
-
-def print_ratings(ratings)
-  ratings.each { |team| puts "#{team[0]}\t#{team[1]}" }
 end
 
 def rating_for_team(team, ratings)
   matches = ratings.map do |line|
-    [line[0], line[1], Text::Levenshtein.distance(team, line[0])]
+    [line.first, line.second, Text::Levenshtein.distance(team, line.first)]
   end
-  matches.sort_by! { |e| e[2] }
-  return [team, matches.first[1].to_f]
+  matches.sort_by! { |e| e.third }
+  return [team, matches.first.second.to_f]
 end
 
 def pick(game, ratings)
@@ -63,19 +56,25 @@ def print_pick(game, pad_width)
 end
 
 
-def main
+def pick_winners
   ratings      = load_ratings
   slate        = load_slate
-  longest_name = (ratings.map { |team| team.first.length }).max
-
   picks        = []
+
   slate.each { |game| picks.push pick game, ratings }
 
   picks.sort_by! { |game| game[:spread] }
 
   picks.each.with_index { |pick, index| pick[:confidence] = index + 1 }
 
-  picks.sort_by! { |game| game[:index] }
+  return { picks: picks.sort_by! { |game| game[:index] },
+           ratings: ratings}
+end
+
+def print_winners(data)
+  ratings = data[:ratings]
+  picks   = data[:picks]
+  longest_name = (ratings.map { |team| team.first.length }).max
 
   puts "****** PICKS ******".center 80
   picks.each { |game| puts print_pick game, longest_name }
@@ -83,10 +82,16 @@ def main
 
   puts "****** BLOWOUTS ******".center 80
 
-  picks.sort_by! { |game| -game[:spread] }
-  picks[0..2].each { |game| puts print_pick game, longest_name }
-
-  return picks
+  results = picks.sort_by { |game| -game[:spread] }
+  results[0..2].each { |game| puts print_pick game, longest_name }
 end
 
-main
+def picks_as_json(picks)
+  picks.to_json
+end
+
+data = pick_winners
+
+print_winners data
+
+#puts picks_as_json data[:picks]
