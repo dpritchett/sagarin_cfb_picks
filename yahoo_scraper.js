c7e2f69e7107235bb19e1ab3f15eebed42a27ec6
Daniel J. Pritchett <dpritchett@gmail.com>
2011-11-01 09:21:53 -0500

    updated UT handlers

--- yahoo_scraper.js
+++ yahoo_scraper.js
@@ -21,8 +21,8 @@ String.prototype.expand_text = function() {
     str = str.replace('Cent.', "Central")
     str = str.replace('W.', "Western")
     str = str.replace('N.C.', "NC")
-    str = str.replace('Tenn-', "Tennessee-")
-    str = str.replace('Middle Tenn. St', "Middle Tennessee")
+    str = str.replace("Tenn-Martin", "Tennessee-Martin")
+    str = str.replace("Middle Tenn. St.", "Middle Tennessee")
 
     str = str.replace(/@|\(\d+\)/gi, '')
     return str
